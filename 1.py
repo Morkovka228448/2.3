d1dc72dacd0ccdd96a92521ba69babf5f0f29efd
Cergey <kurdenevicvladislav448@gmail.com>
2025-05-14 17:29:48 +0700

    Update 1.py

--- 1.py
+++ 1.py
@@ -13,7 +13,7 @@ class Worker:
     def __str__(self):
       return f"Worker: {self.name} {self.surname}, Rate: {self.rate}, Days: {self.days}, Salary: {self.get_salary()}"
 
-worker = Worker("Влад", "Курденевич", 3200, 20)
+worker = Worker("Сергей", "Иванов", 3200, 20)
 salary = worker.get_salary()
 print(f"Зарплата работника: {salary}")
 print(worker)
